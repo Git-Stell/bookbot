7a2b583c891dfe2abe64dca01ee7d29500a81c13
GitStell <adam.szymanski41@proton.me>
2025-08-14 22:19:14 +0200

    Added readding file function

---
+++ main.py
@@ -0,0 +1,9 @@
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    print(f"{get_book_text("./books/frankenstein.txt")}")
+
+main()
