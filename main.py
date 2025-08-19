24077462b094abae2e14b8f7484a7de11873c4db
GitStell <adam.szymanski41@proton.me>
2025-08-19 17:31:10 +0200

    Choosing the book to scan by entering a path to file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from stats import get_book_text, counting, characters_count, dictionary
+import sys
 
 def main():
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    
     print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
-    num_words = counting(get_book_text("./books/frankenstein.txt"))
+    num_words = counting(get_book_text(f"./{sys.argv[1]}"))
     print(f"Found {num_words} total words\n--------- Character Count -------")
 
-    end = characters_count(get_book_text("./books/frankenstein.txt"))
+    end = characters_count(get_book_text(f"./{sys.argv[1]}"))
     dictionary(end)
 
 
